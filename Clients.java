28db3d1eca9553468c1f928b550ac6830b7c56c1
Dat Truong <dtruong5@student.umgc.edu>
2022-11-02 08:24:13 -0700

    Commit and run file

---
+++ Clients.java
@@ -0,0 +1,8 @@
+public class Clients implements Constants{
+    public static void main(String[] args) {
+        double radius = 1.5;
+
+        System.out.printf("Area of circle is %.2f %s%n", PI_APPROXIMATION*radius*radius,AREA_UNITS);
+        System.out.printf("Circumference of circle is %.2f %s%n", 2.0*Constants.PI_APPROXIMATION*radius, Constants.LENGTH_UNIT);
+    }
+}
---
+++ Constants.java
@@ -0,0 +1,6 @@
+public interface Constants {
+    double PI_APPROXIMATION = 3.14;
+    String AREA_UNITS = "sq.cm.";
+    String LENGTH_UNIT = "cm.";
+}
+
